9fa1744a4603309fa2a5f786fa62964bf754503d
Dayana Torres <luzdayanatorres@gmail.com>
2024-11-30 01:33:48 -0500

    Creación de pantallas nuevas

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const vegetables = [
-    { name: "Acelga", img: "imagenes/acelga.jpg", price: "Desde $" },
-    { name: "Ahuyama", img: "imagenes/ahuyama.jpg", price: "Desde $" },
-    { name: "Ahuyama Peruana", img: "imagenes/ahuyama_peruana.jpg", price: "Desde $" },
-    { name: "Alcachofa", img: "imagenes/alcachofa.jpg", price: "Desde $" },
-    { name: "Alfalfa", img: "imagenes/alfalfa.jpg", price: "Desde $" },
-    { name: "Apio", img: "imagenes/apio.jpg", price: "Desde $" },
-    { name: "Arracacha", img: "imagenes/arracacha.jpg", price: "Desde $" },
-    { name: "Arverja Cascara", img: "imagenes/arveja_cascara.jpg", price: "Desde $" },
-    { name: "Arveja Desgranada", img: "imagenes/arveja_desgranada.jpg", price: "Desde $" },
+    { name: "Acelga", img: "imagenes/acelga.jpg", price: "Desde $1.000" },
+    { name: "Ahuyama", img: "imagenes/ahuyama.jpg", price: "Desde $2.000" },
+    { name: "Ahuyama Peruana", img: "imagenes/ahuyama_peruana.jpg", price: "Desde $3.000" },
+    { name: "Alcachofa", img: "imagenes/alcachofa.jpg", price: "Desde $5.000" },
+    { name: "Alfalfa", img: "imagenes/alfalfa.jpg", price: "Desde $6.000" },
+    { name: "Apio", img: "imagenes/apio.jpg", price: "Desde $2.000" },
+    { name: "Arracacha", img: "imagenes/arracacha.jpg", price: "Desde $1.000" },
+    { name: "Arverja Cascara", img: "imagenes/arveja_cascara.jpg", price: "Desde $8.000" },
+    { name: "Arveja Desgranada", img: "imagenes/arveja_desgranada.jpg", price: "Desde $5.600" },
     { name: "Batata", img: "imagenes/batata.jpg", price: "Desde $" },
     { name: "Berenjena", img: "imagenes/berenjena.jpg", price: "Desde $" },
     { name: "Brócoli", img: "imagenes/brocoli.jpg", price: "Desde $" },
@@ -46,6 +46,8 @@ const herbs = [
     { name: "Poleo", img: "imagenes/poleo.jpg", price: "Desde $" },
     { name: "Ruda", img: "imagenes/ruda.jpg", price: "Desde $" }
 ];
+let cart = []; // Array para almacenar los productos en el carrito
+
 
 function showScreen(screen) {
     const homeScreen = document.getElementById('homeScreen');
@@ -56,9 +58,12 @@ function showScreen(screen) {
     const saleOfVegetablesScreen = document.getElementById('saleOfVegetables');
     const saleOfFruitsScreen = document.getElementById('saleOfFruits');
     const saleOfHerbsScreen = document.getElementById('saleOfHerbs');
-    const saleOtherScreen = document.getElementById('saleOther');
+    const saleOtherScreen = document.getElementById('saleOther'); // Pantalla de otras ventas
+    const cartScreen = document.getElementById('cart');
+    const paymenScreen = document.getElementById('paymentScreen');
+    
 
-    // Ocultar todas las pantallas
+    // Oculta todas las pantallas
     homeScreen.style.display = 'none';
     loginScreen.style.display = 'none';
     registerScreen.style.display = 'none';
@@ -67,8 +72,11 @@ function showScreen(screen) {
     saleOfVegetablesScreen.style.display = 'none';
     saleOfFruitsScreen.style.display = 'none';
     saleOfHerbsScreen.style.display = 'none';
-    saleOtherScreen.style.display = 'none';
+    saleOtherScreen.style.display = 'none'; // Asegúrate de ocultar esta pantalla
+    cartScreen.style.display = 'none';
+    paymenScreen.style.display = 'none';
 
+    // Muestra la pantalla correspondiente
     if (screen === 'home') {
         homeScreen.style.display = 'flex';
     } else if (screen === 'login') {
@@ -81,17 +89,126 @@ function showScreen(screen) {
         informationScreen.style.display = 'block'; 
     } else if (screen === 'saleOfVegetables') {
         saleOfVegetablesScreen.style.display = 'block'; 
-        generateVegetableCards(); // Generar tarjetas de verduras
+        generateVegetableCards(); 
     } else if (screen === 'saleOfFruits') {
         saleOfFruitsScreen.style.display = 'block'; 
-        generateFruitCards(); // Generar tarjetas de frutas
+        generateFruitCards(); 
     } else if (screen === 'saleOfHerbs') {
         saleOfHerbsScreen.style.display = 'block'; 
         generateHerbsCards();
-    }  else if (screen === 'saleOther') {
-        saleOther.style.display = 'block'; 
+    } else if (screen === 'saleOther') { // Asegúrate de manejar esta opción
+        saleOtherScreen.style.display = 'block'; 
+    } else if (screen === 'cart') {
+        cartScreen.style.display = 'block'; 
+    }else if (screen === 'paymentScreen') {
+        paymenScreen.style.display = 'block';
     }
 }
+
+function addToCart(product) {
+    const existingProduct = cart.find(item => item.name === product.name);
+    if (existingProduct) {
+        existingProduct.quantity += 1; // Aumentar la cantidad si ya existe
+    } else {
+        product.quantity = 1; // Establecer cantidad inicial
+        cart.push(product);
+    }
+    updateCartDisplay();
+}
+
+
+function updateCartDisplay() {
+    const cartItemsContainer = document.getElementById('cart-items');
+    cartItemsContainer.innerHTML = ''; // Limpiar el contenedor del carrito
+    let total = 0;
+
+    cart.forEach(item => {
+        const div = document.createElement('div');
+        div.classList.add('cart-item');
+        div.innerHTML = `
+            <img src="${item.img}" alt="${item.name}" class="cart-item-img">
+            <span>${item.name}</span>
+            <span>${item.price}</span>
+            <span>Cantidad: ${item.quantity}</span>
+            <button onclick='removeFromCart("${item.name}")'>Eliminar</button>
+            <button onclick='increaseQuantity("${item.name}")'>+</button>
+            <button onclick='decreaseQuantity("${item.name}")'>-</button>
+        `;
+        cartItemsContainer.appendChild(div);
+        total += parseFloat(item.price.replace('Desde $', '').trim()) * item.quantity;
+    });
+
+    document.getElementById('cart-total').innerText = `Total: $${total.toFixed(2)}`;
+
+    // Agregar el botón de pagar
+    const payButton = document.createElement('button');
+    payButton.textContent = 'Pagar';
+    payButton.onclick = showPaymentScreen;
+    cartItemsContainer.appendChild(payButton);
+
+    document.getElementById('cart').style.display = 'block'; // Mostrar el carrito
+}
+
+function showPaymentScreen() {
+    // Ocultar la pantalla del carrito
+    document.getElementById('cart').style.display = 'none';
+
+    // Mostrar la pantalla de pago
+    const paymentScreen = document.getElementById('paymentScreen');
+    paymentScreen.style.display = 'block';
+
+    // Llenar los campos de la pantalla de pago
+    document.getElementById('deliveryAddress').value = 'Calle Ficticia 123, Ciudad, País';
+
+    // Configurar el calendario de fecha de entrega
+    const deliveryDateInput = document.getElementById('deliveryDate');
+    deliveryDateInput.type = 'date';
+    deliveryDateInput.min = new Date().toISOString().split('T')[0]; // Establecer la fecha mínima como la fecha actual
+}
+
+function processPayment() {
+    // Obtener los valores de los campos de pago
+    const paymentMethod = document.querySelector('input[name="paymentMethod"]:checked').value;
+    const cardNumber = document.getElementById('cardNumber').value;
+    const expirationDate = document.getElementById('expirationDate').value;
+    const cvv = document.getElementById('cvv').value;
+
+    // Validar los campos de pago
+    if (!paymentMethod || !cardNumber || !expirationDate || !cvv) {
+        alert('Por favor, completa todos los campos de pago.');
+        return;
+    }
+
+    // Procesar el pago
+    if (paymentMethod === 'card') {
+        // Lógica para procesar el pago con tarjeta de crédito
+        processCardPayment(cardNumber, expirationDate, cvv);
+    } else if (paymentMethod === 'pse') {
+        // Lógica para procesar el pago con PSE
+        processPSEPayment();
+    }
+
+    // Limpiar el carrito y volver a la pantalla principal
+    cart = [];
+    updateCartDisplay();
+    showScreen('main');
+}
+
+function processCardPayment(cardNumber, expirationDate, cvv) {
+    // Aquí deberías implementar la lógica para procesar el pago con una pasarela de pago real
+    console.log('Procesando pago con tarjeta de crédito:');
+    console.log('Número de tarjeta:', cardNumber);
+    console.log('Fecha de expiración:', expirationDate);
+    console.log('CVV:', cvv);
+    alert('Pago con tarjeta de crédito procesado exitosamente.');
+}
+
+function processPSEPayment() {
+    // Aquí deberías implementar la lógica para procesar el pago con PSE
+    console.log('Procesando pago con PSE');
+    alert('Pago con PSE procesado exitosamente.');
+}
+
 
 function generateVegetableCards() {
     const vegetableContainer = document.getElementById('vegetableContainer');
@@ -104,7 +221,7 @@ function generateVegetableCards() {
             <img src="${vegetable.img}" alt="${vegetable.name}">
             <h3>${vegetable.name}</h3>
             <p>${vegetable.price}</p>
-            <button>Agregar al carrito</button>
+            <button onclick='addToCart({ name: "${vegetable.name}", img: "${vegetable.img}", price: "${vegetable.price}" })'>Agregar al carrito</button>
         `;
         vegetableContainer.appendChild(article);
     });
@@ -121,26 +238,50 @@ function generateFruitCards() {
             <img src="${fruit.img}" alt="${fruit.name}">
             <h3>${fruit.name}</h3>
             <p>${fruit.price}</p>
-            <button>Agregar al carrito</button>
+            <button onclick='addToCart({ name: "${fruit.name}", img: "${fruit.img}", price: "${fruit.price}" })'>Agregar al carrito</button>
         `;
         fruitContainer.appendChild(article);
     });
 }
+
 function generateHerbsCards() {
     const herbsContainer = document.getElementById('containerHerbs');
     herbsContainer.innerHTML = ''; // Limpiar el contenedor antes de agregar nuevas tarjetas
 
-    herbs.forEach(herbs => {
+    herbs.forEach(herb => {
         const article = document.createElement('article');
         article.classList.add('cards');
         article.innerHTML = `
-            <img src="${herbs.img}" alt="${herbs.name}">
-            <h3>${herbs.name}</h3>
-            <p>${herbs.price}</p>
-            <button>Agregar al carrito</button>
+            <img src="${herb.img}" alt="${herb.name}">
+            <h3>${herb.name}</h3>
+            <p>${herb.price}</p>
+            <button onclick='addToCart({ name: "${herb.name}", img: "${herb.img}", price: "${herb.price}" })'>Agregar al carrito</button>
         `;
         herbsContainer.appendChild(article);
     });
+}
+
+function removeFromCart(productName) {
+    cart = cart.filter(item => item.name !== productName);
+    updateCartDisplay();
+}
+
+function increaseQuantity(productName) {
+    const product = cart.find(item => item.name === productName);
+    if (product) {
+        product.quantity += 1;
+        updateCartDisplay();
+    }
+}
+
+function decreaseQuantity(productName) {
+    const product = cart.find(item => item.name === productName);
+    if (product && product.quantity > 1) {
+        product.quantity -= 1;
+        updateCartDisplay();
+    } else if (product) {
+        removeFromCart(productName); // Eliminar si la cantidad es 1
+    }
 }
 
 function login() {
